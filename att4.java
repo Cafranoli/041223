84f0d9a344f6790e1b7d8917c33fec91e0de1a8f
Cafranoli <146767947+Cafranoli@users.noreply.github.com>
2023-12-05 14:48:26 -0300

    Add files via upload

---
+++ att4.java
@@ -0,0 +1,32 @@
+package attSWITCHCASE;
+
+import java.util.Scanner;
+
+public class att4 {                 //direcionar sala de cinema para crianças de 1 a 5 anos, conforme idade
+    public static void main(String[] args) {
+        Scanner scnIdade = new Scanner(System.in);
+        System.out.println("Digite sua idade");
+        short idade = scnIdade.nextShort();
+
+        switch (idade) {
+            case 1:
+                System.out.println("Por favor dirija-se à sala 1");
+                break;
+            case 2:
+                System.out.println("Por favor dirija-se à sala 2");
+                break;
+            case 3:
+                System.out.println("Por favor dirija-se à sala 3");
+                break;
+            case 4:
+                System.out.println("Por favor dirija-se à sala 4");
+                break;
+            case 5:
+                System.out.println("Por favor dirija-se à sala 5");
+                break;
+            default:
+                System.out.println("Idade inválida");
+                break;
+        }
+    }
+}
